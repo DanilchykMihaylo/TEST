2016bc84b67413e5efa2ad706c6a82fa8dab06ea
DanilchukMihaylo <ipz243_dmb@student.ztu.edu.ua>
2025-11-03 20:29:27 +0200

    main ===

--- script3.js
+++ script3.js
@@ -3,7 +3,7 @@ const tableDivs = document.querySelectorAll('.table');
 tableDivs.forEach(div => {
     const tds = div.querySelectorAll('td');
     tds.forEach((td, index) => {
-        if ((index + 1) % 2 == 0) {
+        if ((index + 1) % 2 === 0) {
             td.classList.add('selected');
         }
     });
